eaa6396b86124d6dcf1cd8b9d06c81a2fac190c2
Max Saienko <msaienko@ancestry.com>
2016-08-29 23:26:20 -0700

    add more files

---
+++ Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  happyt
+//
+//  Created by Max Saienko on 8/29/16.
+//  Copyright © 2016 Max Saienko. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+#endif /* Bridging_Header_h */
