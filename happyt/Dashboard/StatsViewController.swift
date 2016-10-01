9e156b632e15de9f4a80e1384df9d92deb32766a
Max Saienko <msaienko@ancestry.com>
2016-10-01 01:55:25 -0700

    first chart

--- happyt/Dashboard/StatsViewController.swift
+++ happyt/Dashboard/StatsViewController.swift
@@ -30,145 +30,60 @@ class StatsViewController: UIViewController {
             contentView.addSubview(container.view)
             offsetY = 10 + container.view.frame.maxY
             
-            let chartConfig = BarsChartConfig(
-                valsAxisConfig: ChartAxisConfig(from: 0, to: 8, by: 2)
+            // Chart
+            let chartFrame = CGRectMake(10, 40, containerWidth - 20, containerHeight - 45)
+            
+            let chartPoints: [ChartPoint] = [(2, 2), (4, -4), (6, 6), (8, 10), (12, 9)].map{ChartPoint(x: ChartAxisValueInt($0.0), y: ChartAxisValueInt($0.1))}
+            
+            let xValues = 0.stride(through: 24, by: 3).map {ChartAxisValueInt($0)}
+            //let xValues = [ChartAxisValueInt(-2), ChartAxisValueInt(0), ChartAxisValueInt(3), ChartAxisValueInt(5)]
+            let yValues = (-4).stride(through: 10, by: 2).map {ChartAxisValueInt($0)}
+            
+            let labelSettings = ChartLabelSettings(font: UIFont.systemFontOfSize(14))
+            
+            // create axis models with axis values and axis title
+            let xModel = ChartAxisModel(axisValues: xValues, axisTitleLabel: ChartAxisLabel(text: "Time", settings: labelSettings))
+            let yModel = ChartAxisModel(axisValues: yValues, axisTitleLabel: ChartAxisLabel(text: "Count", settings: labelSettings.defaultVertical()))
+            
+            let chartSettings = ChartSettings()
+            chartSettings.axisStrokeWidth = 0.2
+            chartSettings.top = 10
+            chartSettings.trailing = 10
+            
+            let coordsSpace = ChartCoordsSpaceLeftBottomSingleAxis(chartSettings: chartSettings, chartFrame: chartFrame, xModel: xModel, yModel: yModel)
+            let (xAxis, yAxis, innerFrame) = (coordsSpace.xAxis, coordsSpace.yAxis, coordsSpace.chartInnerFrame)
+            
+            // create layer with line
+            let lineModel = ChartLineModel(chartPoints: chartPoints, lineColor: UIColor(red: 0.4, green: 0.4, blue: 1, alpha: 0.2), lineWidth: 3, animDuration: 0.7, animDelay: 0)
+            let chartPointsLineLayer = ChartPointsLineLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, lineModels: [lineModel])
+            
+            // create layer with base line
+            let baseLinePoints: [ChartPoint] = [(0, 0), (24, 0)].map{ChartPoint(x: ChartAxisValueInt($0.0), y: ChartAxisValueInt($0.1))}
+            let baseLineModel = ChartLineModel(chartPoints: baseLinePoints, lineColor: UIColor.blueColor(), lineWidth: 3, animDuration: 0, animDelay: 0)
+            let baseLineChartPointsLineLayer = ChartPointsLineLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, lineModels: [baseLineModel])
+            
+            // create layer with guidelines
+            let settings = ChartGuideLinesDottedLayerSettings(linesColor: UIColor.blackColor(), linesWidth: 1.0)
+            let guidelinesLayer = ChartGuideLinesDottedLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, settings: settings)
+            
+            let chart = Chart(
+                frame: chartFrame,
+                layers: [
+                    xAxis,
+                    yAxis,
+                    guidelinesLayer,
+                    chartPointsLineLayer,
+                    baseLineChartPointsLineLayer
+                ]
             )
             
-            let chart = BarsChart(
-                frame: CGRectMake(10, 40, containerWidth - 20, containerHeight - 45),
-                chartConfig: chartConfig,
-                xTitle: "X axis",
-                yTitle: "Y axis",
-                bars: [
-                    ("A", 2),
-                    ("B", 4.5),
-                    ("C", 3),
-                    ("D", -5.4),
-                    ("E", 6.8),
-                    ("F", 0.5)
-                ],
-                color: UIColor.redColor(),
-                barWidth: 20
-            )
+            
+            
+            //////////////==================////////////////
             
             self.charts.append(chart)
             container.view.addSubview(chart.view)
         }
-        
-        
-//        let chartConfig = ChartConfigXY(
-//            xAxisConfig: ChartAxisConfig(from: 2, to: 14, by: 2),
-//            yAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 2)
-//        )
-//        
-//        let chart = LineChart(
-//            frame: CGRectMake(0, 70, 300, 500),
-//            chartConfig: chartConfig,
-//            xTitle: "X axis",
-//            yTitle: "Y axis",
-//            lines: [
-//                (chartPoints: [(2.0, 10.6), (4.2, 5.1), (7.3, 3.0), (8.1, 5.5), (14.0, 8.0)], color: UIColor.redColor()),
-//                (chartPoints: [(2.0, 2.6), (4.2, 4.1), (7.3, 1.0), (8.1, 11.5), (14.0, 3.0)], color: UIColor.blueColor())
-//            ]
-//        )
-//        
-//        self.view.addSubview(chart.view)
-        // Do any additional setup after loading the view.
-        
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        let chartPoints: [ChartPoint] = [(2, 2), (4, 4), (6, 6), (8, 10), (12, 14)].map{ChartPoint(x: ChartAxisValueInt($0.0), y: ChartAxisValueInt($0.1))}
-//        
-//        let xValues = 0.stride(through: 16, by: 2).map {ChartAxisValueInt($0)}
-//        let yValues = 0.stride(through: 16, by: 2).map {ChartAxisValueInt($0)}
-//        
-//        let labelSettings = ChartLabelSettings(font: UIFont.systemFontOfSize(14))
-//        
-//        // create axis models with axis values and axis title
-//        let xModel = ChartAxisModel(axisValues: xValues, axisTitleLabel: ChartAxisLabel(text: "Axis title", settings: labelSettings))
-//        let yModel = ChartAxisModel(axisValues: yValues, axisTitleLabel: ChartAxisLabel(text: "Axis title", settings: labelSettings.defaultVertical()))
-//        
-//        let chartFrame = CGRectMake(20, 100, 300, 400)
-//        
-//        let chartSettings = ChartSettings()
-//        chartSettings.axisStrokeWidth = 0.2
-//        chartSettings.top = 20
-//        chartSettings.trailing = 20
-//        // ...
-//        
-//        let coordsSpace = ChartCoordsSpaceLeftBottomSingleAxis(chartSettings: chartSettings, chartFrame: chartFrame, xModel: xModel, yModel: yModel)
-//        let (xAxis, yAxis, innerFrame) = (coordsSpace.xAxis, coordsSpace.yAxis, coordsSpace.chartInnerFrame)
-//        
-//        // create layer with line
-//        let lineModel = ChartLineModel(chartPoints: chartPoints, lineColor: UIColor(red: 0.4, green: 0.4, blue: 1, alpha: 0.2), lineWidth: 3, animDuration: 0.7, animDelay: 0)
-//        let chartPointsLineLayer = ChartPointsLineLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, lineModels: [lineModel])
-//        
-//        // creates custom view for each chartpoint
-//        let myCustomViewGenerator = {(chartPointModel: ChartPointLayerModel, layer: ChartPointsLayer, chart: Chart) -> UIView? in
-//            let center = chartPointModel.screenLoc
-//            let label = UILabel(frame: CGRectMake(center.x - 20, center.y - 10, 40, 20))
-//            label.backgroundColor = UIColor.greenColor()
-//            label.textAlignment = NSTextAlignment.Center
-//            label.text = chartPointModel.chartPoint.description
-//            //label.font = ExamplesDefaults.labelFont
-//            return label
-//        }
-//        
-//        // create layer that uses the view generator
-//        let myCustomViewLayer = ChartPointsViewsLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, chartPoints: chartPoints, viewGenerator: myCustomViewGenerator, displayDelay: 0, delayBetweenItems: 0.05)
-//        
-//        
-//        // create layer with guidelines
-//        let settings = ChartGuideLinesDottedLayerSettings(linesColor: UIColor.blackColor(), linesWidth: 1.0)
-//        let guidelinesLayer = ChartGuideLinesDottedLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, settings: settings)
-//        
-//        let chart = Chart(
-//            frame: chartFrame,
-//            layers: [
-//                xAxis,
-//                yAxis,
-//                guidelinesLayer,
-//                chartPointsLineLayer,
-//                myCustomViewLayer
-//            ]
-//        )
-//        
-//        self.view.addSubview(chart.view)
-//        self.chart = chart
-        
-        let chartConfig = BarsChartConfig(
-            valsAxisConfig: ChartAxisConfig(from: 0, to: 8, by: 2)
-        )
-        
-        let chart = BarsChart(
-            frame: CGRectMake(0, 0, 300, 300),
-            chartConfig: chartConfig,
-            xTitle: "X axis",
-            yTitle: "Y axis",
-            bars: [
-                ("A", 2),
-                ("B", 4.5),
-                ("C", 3),
-                ("D", 5.4),
-                ("E", 6.8),
-                ("F", 0.5)
-            ],
-            color: UIColor.redColor(),
-            barWidth: 20
-        )
-        
-        //chartView.addSubview(chart.view)
-        //self.chart = chart
     }
 
     override func didReceiveMemoryWarning() {
